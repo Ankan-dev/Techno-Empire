5991fa20164c7a1085cf6b800acf36980318cdcb
Ankan Mandal <ankanmandal2001@gmail.com>
2024-09-22 14:20:40 +0530

    changes made

--- frontend/src/routes/VerifyUser.jsx
+++ frontend/src/routes/VerifyUser.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { addUser } from '../slice/user-slice.js'
-
+import './style-folder/style.css'
 
 const VerifyUser = () => {
 
@@ -96,16 +96,16 @@ const VerifyUser = () => {
             <h1 className='text-2xl font-bold md:text-5xl md:mt-10 text-white'>Enter the Code sent to your mail</h1>
             <form className='w-full min-h-[15vh]  flex flex-col items-center justify-center mt-5 gap-3 '>
                 <input type='password' value={code} onChange={(e) => { handleChange(e) }} placeholder='Enter the code' className='w-[80%] h-10 border-2  p-2 md:w-[30%] border-blue-400 bg-transparent' />
-                <button className='bg-black text-white font-bold w-[10rem] h-[2.5rem]' onClick={handleSubmit}>Verify</button>
+                <button id='verify-button' className='border-2 border-white rounded-lg text-white font-bold w-[10rem] h-[2.5rem]' onClick={handleSubmit}>Verify</button>
                 {
                     timeOver ?
-                        <button className=' border-black border-solid border-2 font-bold w-[10rem] h-[2.5rem] my-5' onClick={resentCode}>Resend Code</button>
+                        <button className=' border-white border-solid border-2 font-bold w-[10rem] h-[2.5rem] my-5 text-white' onClick={resentCode}>Resend Code</button>
                         : <></>
                 }
             </form>
 
-            <div className=' w-[5rem] h-[5rem] border-black border-4 rounded-full flex justify-center items-center mt-4'>
-                <h1 className='font-bold text-3xl'>{counter}</h1>
+            <div className=' w-[5rem] h-[5rem] border-white border-4 rounded-full flex justify-center items-center mt-4'>
+                <h1 className='font-bold text-3xl text-white'>{counter}</h1>
             </div>
         </div>
     )
